d973b6ce06c34672087866992286a744c97fed81
Onyx McQueen <onyxmcqueen88@gmail.com>
2025-08-12 20:14:39 -0400

    Add GameController module with turn handling, win detection, and game state management

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Create and return a game board object that is invoked using the module pattern.
-
+// Game Board Module
 const gameBoardModule = (function() {
   let gameBoard = ["", "", "", "", "", "", "", "", ""];
 
@@ -9,16 +8,13 @@ const gameBoardModule = (function() {
 
   function placeMark(index, mark){
     if(index < 0 || index > 8){
-      console.log('Error: index out of bounds');
       return false;
     }
-
     if(gameBoard[index] === ""){
       gameBoard[index] = mark;
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
 
   function resetBoard(){
@@ -32,12 +28,89 @@ const gameBoardModule = (function() {
   }
 })();
 
-// Factory function that will create player objects with public methods that
-// interact with name and mark private data
+//Player Factory
 
 function createPlayer(name, mark){
-  return {
-    getName: () => name,
-    getMark: () => mark
-  }
+  return { name, mark };
 }
+
+// Game Controller Module
+const gameControllerModule = (function() {
+  let players = [];
+  let currentPlayerIndex = 0;
+  let roundCount = 0;
+  let gameOver;
+  let winner = null;
+
+  const winningCombinations = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+  [0, 4, 8], [2, 4, 6]             // diagonals
+  ];
+
+  function isWinner(playerMark){
+    const board = gameBoardModule.getBoard();
+    return winningCombinations.find(([a, b, c]) => {
+      return board[a] === playerMark && board[b] === playerMark && board[c] === playerMark
+    })
+  }
+
+  function checkGameStatus(playerMark){
+    const combo = isWinner(playerMark);
+    if(combo){
+      winner = players[currentPlayerIndex];
+      gameOver = true;
+      return { status: 'win', data: { winner, combo } }
+    }
+    if(roundCount === 9){
+      gameOver = true;
+      return { status: 'tie' };
+    }
+
+    return { status: 'continue' };
+  }
+
+  function startGame(){
+    players = [
+      createPlayer('Player 1', 'X'), 
+      createPlayer('Player 2', 'O')
+    ];
+    gameBoardModule.resetBoard();
+    currentPlayerIndex = 0;
+    roundCount = 0;
+    winner = null;
+    gameOver = false;
+  }
+
+  function playTurn(index){
+    if(gameOver) return { status: 'game-over' };
+
+    const currentPlayer = players[currentPlayerIndex];
+    const isPlaced = gameBoardModule.placeMark(index, currentPlayer.mark);
+
+    if(!isPlaced) return { status: "invalid" };
+
+    roundCount++;
+    const statusResult = checkGameStatus(currentPlayer.mark);
+    if(statusResult.status !== "continue") return statusResult;
+
+    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    return { status: 'continue' };
+  }
+
+  function getCurrentPlayer(){
+    return players[currentPlayerIndex];
+  }
+
+  function getWinner(){
+    return winner;
+  }
+
+
+  return {
+    startGame,
+    playTurn,
+    getCurrentPlayer,
+    getWinner
+  }
+})();
